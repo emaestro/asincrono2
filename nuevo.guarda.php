f4eb7d50aa5724f0f1ad3dcc40f4a7248cfd1c73
emaestro <f@emaestro.net>
2015-09-29 19:51:00 -0500

    Iniciamos la implementacion de nuevo.guarda.php

--- nuevo.guarda.php
+++ nuevo.guarda.php
@@ -1,4 +1,12 @@
 <?php
 
-	print_r($_POST);
+	//print_r($_POST);
+
+	//Por aqui hacer el require de db.php
+
+	$sSQL	=	"INSERT INTO usuarios SET "
+			.	"paterno = '{$_POST['txt-paterno']}', "
+			.	"materno = '{$_POST['txt-materno']}' "
+
+	//Luego que tenemos $sSQL ejecutamos con $conexion->query($sSQL)
 ?>
